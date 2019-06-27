8c9d788918eb0582307322036c7e1b03a91742ae
pawandeep1999 <52276873+pawandeep1999@users.noreply.github.com>
2019-06-27 01:53:07 -0700

    routes,.py

---
+++ routes.py
@@ -0,0 +1,14 @@
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+  user = {'username': 'SEHJALPREET'}
+ return '''
+<html>
+ <head>
+ <title>Home Page - Microblog</title>
+ </head>
+ <body>
+ <h1>Hello, ''' + user['username'] + '''!</h1>
+ </body>
+</html>'''
